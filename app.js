966bf566003a6cf29ad1a5253dda02a869bfc10b
Michael Cordero <magcordero@ucdavis.edu>
2021-07-12 23:24:13 -0700

    updated git ignore

--- app.js
+++ app.js
@@ -21,5 +21,5 @@ app.use("/posts", post);
 app.use("/delete", deletePost);
 
 app.listen(process.env.PORT || 3000, function() {
-  console.log("Server started on port 3000");
+  console.log("Server started successfully");
 });
